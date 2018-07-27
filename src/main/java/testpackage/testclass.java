74389643cca4c75e04e311a6cc9796a859556f23
sornakiruthika <sornakiruthika@192.168.1.26>
2018-07-27 09:51:42 +0530

    comitting test case

---
+++ src/main/java/testpackage/testclass.java
@@ -0,0 +1,16 @@
+package testpackage;
+
+import org.testng.annotations.Test;
+
+public class testclass {
+
+	@Test
+	public void test1() {
+		System.out.println("test1");
+	}
+	
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+}
